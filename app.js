87fb60c2e06958ace285ba83dc9192a8d0197c6f
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-09-14 02:55:35 +0900

    app 1.5

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ npm install  --save express
 
 const express = require("express");
 const app = express();
-
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json([{
-        tele: "shin",
-    }
-    ]);
+    res.json(data
+    );
 });
 app.listen(port, () => console.log('None'));
