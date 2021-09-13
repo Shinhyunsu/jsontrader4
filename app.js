283509d466dd8084af22e606ca777fca12539b63
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-09-14 03:02:44 +0900

    app 1.6

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json(data
-    );
+    /*res.json(data
+    );*/
+    res.send('noargef');
 });
 app.listen(port, () => console.log('None'));
