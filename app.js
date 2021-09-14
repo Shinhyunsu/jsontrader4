99b0c5b3238f3504858fe9a766c2593feed32258
신현수 <sinhyeonsu@sinhyeonsuui-MacBook-Pro.local>
2021-09-14 11:57:22 +0900

    app 1.8

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ npm install  --save express
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const cors = require('cors');
 const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.get("/", (req, res) => {
     res.json(data
     );
